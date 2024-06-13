cbf734f0cb2c2990b10f624f71a19c220914bb10
longvv <longvv@gmail.com>
2024-06-13 18:50:33 +0700

    fixed dependencies

--- src/pages/MyChat.tsx
+++ src/pages/MyChat.tsx
@@ -1,12 +1,14 @@
-import { EmbeddedChat } from '@embeddedchat/react';
+// import { EmbeddedChat } from '@embeddedchat/react';
 
 export default function MyChat() {
     return (
         <div>
             <div style={{height: "100vh"}}>
-                <EmbeddedChat 
+                {/* <EmbeddedChat 
                     host="https://ff953t94-3000.asse.devtunnels.ms/"
                     />
+                 */}
+                Test Chat
             </div>
                 
         </div>
